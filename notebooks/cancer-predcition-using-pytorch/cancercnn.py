ecd710d3340a5423b7c6826898c2affd866f743b
Azran Hossain <mr69751@gmail.com>
2023-04-17 20:13:53 +0600

    separate module for cnn

---
+++ notebooks/cancer-predcition-using-pytorch/cancercnn.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+
+
+class CancerCNN(nn.Module):
+    def __init__(self):
+        super(CancerCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(256 * 28 * 28, 512)
+        self.fc2 = nn.Linear(512, 2)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.pool(x)
+        x = self.relu(self.conv2(x))
+        x = self.pool(x)
+        x = self.relu(self.conv3(x))
+        x = self.pool(x)
+        x = x.view(-1, 256 * 28 * 28)
+        x = self.dropout(self.relu(self.fc1(x)))
+        x = self.fc2(x)
+        return x
